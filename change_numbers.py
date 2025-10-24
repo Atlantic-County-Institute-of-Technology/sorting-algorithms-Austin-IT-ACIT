d39976dbe92050df716019426fe06f8f9ef4ffe2
Austin <vane9936@student.acitech.org>
2025-10-24 10:47:53 -0400

    Finished

---
+++ change_numbers.py
@@ -0,0 +1,5 @@
+def change(min_numbers, max_numbers, amount_numbers):
+    min_numbers = int(input(" [-] what is the lowest number wanted to see?"))
+    max_numbers = int(input(" [-] what is the highest number wanted to see?"))
+    amount_numbers = int(input(" [-] what is the amount of number(s) wanted to see?"))
+    return min_numbers, max_numbers, amount_numbers
