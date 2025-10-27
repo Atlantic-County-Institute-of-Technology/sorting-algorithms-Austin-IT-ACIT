617ccf400704aedda31f5c9fc4688988c28063cd
Austin <vane9936@student.acitech.org>
2025-10-27 09:51:07 -0400

    added comments / finished

--- Vaneman_main.py
+++ Vaneman_main.py
@@ -12,6 +12,7 @@ from selection_sort import selection
 
 from change_numbers import change
 
+# start of the code / project
 print(input(" [-] welcome to my sorting numbers project \n [-] Press enter to begin"))
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -20,9 +21,10 @@ temp_numbers = []
 numbers = []
 
 
-
+# main menu code
 def main():
 
+    # all variables for code
     selection_1 = 0
     selection_2 = 0
     shown = 0
@@ -37,6 +39,8 @@ def main():
     # amount_numbers = int(input(" [-] what is the amount of number(s) wanted to see?"))
     numbers = [random.randint(min_numbers, max_numbers) for i in range(amount_numbers)]
     temp_numbers = str(numbers)
+
+    # start of main menu code
     while True:
 
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -51,30 +55,41 @@ def main():
             os.system('cls' if os.name == 'nt' else 'clear')
             exit()
 
+        # change numbers
         if selection_1 == 1:
             os.system('cls' if os.name == 'nt' else 'clear')
             change(min_numbers, max_numbers, amount_numbers)
+
+        # go to the sorting menu
         if selection_1 == 2:
             sec_menu()
 
 
+# second menu code / sorting menu
 def sec_menu():
 
+    # all variables for code
     selection_1 = 0
     selection_2 = 0
     shown = 0
     menu = 0
 
+    # start of second menu code
     os.system('cls' if os.name == 'nt' else 'clear')
     print(" [-] Select a sorting from this page \n [-] 0. Back to Menu \n [-] 1. Bubble Sorting \n"
           " [-] 2. Insertion Sorting \n [-] 3. Selection Sorting")
     selection_2 = int(input(" [-] Please enter an number: "))
+
+    # goes back to the main menu
     if selection_2 == 0:
         os.system('cls' if os.name == 'nt' else 'clear')
         main()
+
+    # asking if they want to see what the code does
     if selection_2 == 1 or 2 or 3:
         shown = int(input(" [-] do you want to see the process? (0 - No, 1 - Yes)"))
 
+    # code for bubble sorting
     if selection_2 == 1:
         os.system('cls' if os.name == 'nt' else 'clear')
         print(f"Unsorted List: {temp_numbers}")
@@ -82,6 +97,7 @@ def sec_menu():
         print(numbers)
         menu = int(input(" [-] Back to Main Menu (0) or Back to Sort Menu (1)"))
 
+    # code for insertion sorting
     if selection_2 == 2:
         os.system('cls' if os.name == 'nt' else 'clear')
         print(temp_numbers)
@@ -89,6 +105,7 @@ def sec_menu():
         print(numbers)
         menu = int(input(" [-] Back to Main Menu (0) or Back to Sort Menu (1)"))
 
+    # code for selection sorting
     if selection_2 == 3:
         os.system('cls' if os.name == 'nt' else 'clear')
         print(temp_numbers)
@@ -96,10 +113,12 @@ def sec_menu():
         print(numbers)
         menu = int(input(" [-] Back to Main Menu (0) or Back to Sort Menu (1)"))
 
+    # after each sort asked if wanting to go to main menu
     if menu == 0:
         os.system('cls' if os.name == 'nt' else 'clear')
         main()
 
+    # after each sort asked if wanting to go to second menu / sorting menu
     if menu == 1:
         sec_menu()
 
